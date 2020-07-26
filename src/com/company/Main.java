253dac9c9a74f908b1ae66aa6e40bc466dd6b725
bia-programming <bia.pasca@yahoo.com>
2020-07-26 16:42:21 +0300

    Am adaugat problema

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i,j;
+        for(i=0;i<=9;i++)
+            for(j=0;j<=9;j++) {
+                System.out.println("2" +i +"4" +j);
+            }
+    }
+}
